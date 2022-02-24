c17edabfb9dcc8bf1954bb1fe1513d900980f2a4
Alberto Chávez [GDL] <dchavez@compusoluciones.com>
2022-02-24 12:42:18 -0600

    update project add path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,9 @@
-// const path = require('path');
+const path = require('path');
 
 module.exports = {
   i18n: {
     defaultLocale: 'es',
     locales: ['en-US', 'es'],
   },
-  // localePath: path.resolve('./public/locales'),
+  localePath: path.resolve('./public/locales'),
 };
